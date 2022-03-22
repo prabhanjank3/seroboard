ebc4cb6dcce11ef7f49f52aedf0392ef759153ab
omkarsutar <omkar.sutar@apisero.com>
2022-03-22 18:44:38 +0530

    Baseline Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "./components/Login";
+import { useRoutes } from "react-router-dom";
+import Themeroutes from "./routes/Router";
+
+function App() {
+  const routing = useRoutes(Themeroutes);
+  return (
+    <div>
+      <div className="dark">{routing}</div>;
+      <div className="g-signin">
+        <Login />
+      </div>
+    </div>
+  );
+}
+
+export default App;
