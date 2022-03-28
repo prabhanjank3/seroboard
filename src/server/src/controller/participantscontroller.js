f2299b23c82a42f38cb1aa5d3783f396151b8e4f
Binayak921 <binayak.b.mohanty@apisero.com>
2022-03-28 10:15:23 +0530

    Added participant backend functionality

--- src/server/src/controller/participantscontroller.js
+++ src/server/src/controller/participantscontroller.js
@@ -7,14 +7,22 @@ const insertParticipant = (req, resp) => {
 const getAllParticipants = (req,resp) => {
     partdao.getAllParticipants(resp);
 };
-
+const getParticipantDetails = async (req,resp) => {
+    partdao.getParticipantByCondition({participantid: req.params.id}, resp)
+};
 const deleteParticipant = async (req, resp) => {
     partdao.deleteParticipant(req.params.id, resp);
 };
+const updateParticipant = async (req, resp) => {
+    partdao.updateParticipant({participantid: req.params.id}, req.body, resp);
+}
 
 module.exports.insertParticipant = insertParticipant;
 module.exports.getAllParticipants = getAllParticipants;
+module.exports.getParticipantDetails = getParticipantDetails;
 module.exports.deleteParticipant = deleteParticipant;
+module.exports.updateParticipant = updateParticipant;
+
 
 
 
